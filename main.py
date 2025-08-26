ce71a7e232f42b5266c1adbec8edd0582febded2
皮贝贝 <1131591792@qq.com>
2025-08-26 11:29:32 +0800

    fix some bugs

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ async def say_hello(name: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=1234, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=1234, reload=True)
