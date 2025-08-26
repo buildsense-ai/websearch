24cd51e8fbdf656a2326e3764641ee8958e44b00
皮贝贝 <1131591792@qq.com>
2025-08-26 11:25:36 +0800

    修改端口为1234 port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from router.xfyun_websearch import router as xfyun_router
+import uvicorn
 
 app = FastAPI()
 
@@ -15,3 +16,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=1234, reload=True)
